c6278dcd07f80e4f6fc75d4fb040e9b08b745008
babel202 <105557733+babel202@users.noreply.github.com>
2022-12-02 12:05:52 +0300

    upd requirements.txt

--- main.py
+++ main.py
@@ -300,15 +300,6 @@ with st.echo(code_location="below"):
     st.markdown("Я не Артемий Лебедев, но и Вы не Яндекс.")
     st.markdown("Теперь найдем кратчайщий путь от какой-ниюудь станции до любой другой. Например, от Кузбминок до Чкаловской.")
     
-    st.code(
-    """source = uid['/wiki/%D0%A2%D0%B5%D0%BA%D1%81%D1%82%D0%B8%D0%BB%D1%8C%D1%89%D0%B8%D0%BA%D0%B8_(%D1%81%D1%82%D0%B0%D0%BD%D1%86%D0%B8%D1%8F_%D0%BC%D0%B5%D1%82%D1%80%D0%BE,_%D0%A2%D0%B0%D0%B3%D0%B0%D0%BD%D1%81%D0%BA%D0%BE-%D0%9A%D1%80%D0%B0%D1%81%D0%BD%D0%BE%D0%BF%D1%80%D0%B5%D1%81%D0%BD%D0%B5%D0%BD%D1%81%D0%BA%D0%B0%D1%8F_%D0%BB%D0%B8%D0%BD%D0%B8%D1%8F)']
-    target = uid['/wiki/%D0%A7%D0%BA%D0%B0%D0%BB%D0%BE%D0%B2%D1%81%D0%BA%D0%B0%D1%8F_(%D1%81%D1%82%D0%B0%D0%BD%D1%86%D0%B8%D1%8F_%D0%BC%D0%B5%D1%82%D1%80%D0%BE,_%D0%9C%D0%BE%D1%81%D0%BA%D0%B2%D0%B0)']
-    nx.shortest_path_length(G, source=source, target=target) #5"""
-    )
-    source = uid['/wiki/%D0%A2%D0%B5%D0%BA%D1%81%D1%82%D0%B8%D0%BB%D1%8C%D1%89%D0%B8%D0%BA%D0%B8_(%D1%81%D1%82%D0%B0%D0%BD%D1%86%D0%B8%D1%8F_%D0%BC%D0%B5%D1%82%D1%80%D0%BE,_%D0%A2%D0%B0%D0%B3%D0%B0%D0%BD%D1%81%D0%BA%D0%BE-%D0%9A%D1%80%D0%B0%D1%81%D0%BD%D0%BE%D0%BF%D1%80%D0%B5%D1%81%D0%BD%D0%B5%D0%BD%D1%81%D0%BA%D0%B0%D1%8F_%D0%BB%D0%B8%D0%BD%D0%B8%D1%8F)']
-    target = uid['/wiki/%D0%A7%D0%BA%D0%B0%D0%BB%D0%BE%D0%B2%D1%81%D0%BA%D0%B0%D1%8F_(%D1%81%D1%82%D0%B0%D0%BD%D1%86%D0%B8%D1%8F_%D0%BC%D0%B5%D1%82%D1%80%D0%BE,_%D0%9C%D0%BE%D1%81%D0%BA%D0%B2%D0%B0)']
-    nx.shortest_path_length(G, source=source, target=target) #5
-    st.markdown("Получаем, что нужно проехать минимум 5 станций.")
     st.markdown("### Спасибо за внимание!")
     
     
